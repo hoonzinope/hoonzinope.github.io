0f2d85e4ad96ec77b84d811d4bdb3cb23060f870
hoonzi <chap100@naver.com>
2019-06-04 00:42:30 +0900

    modify

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var server = require('https').createServer(app);
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 443;
+var port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
